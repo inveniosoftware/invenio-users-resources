4068ab0833b25637033dd8bb967a3e538925f21d
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2022-02-04 12:48:28 +0100

    users: index email in a different field based on visibility
    * if the user.access.email_visibility is set to 'public', it will be
      indexed in the searchable 'email' field
    * otherwise, it will be indexed in the non-searchable field
      'email_hidden'

---
+++ invenio_users_resources/records/dumpers/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 TU Wien.
+#
+# Invenio-Users-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Elasticsearch dumpers, for transforming to and from versions to index."""
+
+from .email import EmailFieldDumperExt
+
+__all__ = "EmailFieldDumperExt"
