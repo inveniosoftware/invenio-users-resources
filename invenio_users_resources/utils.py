d994660f435f79d4453c469cb908406d4616a2d6
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2022-02-28 14:56:36 +0100

    reindexing: check if the index exists before trying to reindex
    * this fixes an issue during 'invenio-cli services setup', where the
      fixtures (including a role) would be created before the indices were
      initialized, causing the automatic creation of the index
    * this would be picked up and rejected during the actual step of
      'invenio index init'

--- invenio_users_resources/utils.py
+++ invenio_users_resources/utils.py
@@ -8,29 +8,48 @@
 
 """Utility functions."""
 
+from elasticsearch.exceptions import ConflictError
+from flask import current_app
+
 from .proxies import current_groups_service, current_users_service
 from .records.api import GroupAggregate, UserAggregate
 
 
 def reindex_user(user):
     """Reindex the given user."""
-    user_agg = UserAggregate.from_user(user)
-    current_users_service.indexer.index(user_agg)
+    if current_users_service.record_cls.index.exists():
+        try:
+            user_agg = UserAggregate.from_user(user)
+            current_users_service.indexer.index(user_agg)
+        except ConflictError as e:
+            current_app.logger.warn(f"Could not reindex user {user.id}: {e}")
 
 
 def unindex_user(user):
     """Delete the given user from the index."""
-    user_agg = UserAggregate.from_user(user)
-    current_users_service.indexer.delete(user_agg)
+    if current_users_service.record_cls.index.exists():
+        try:
+            user_agg = UserAggregate.from_user(user)
+            current_users_service.indexer.delete(user_agg)
+        except ConflictError as e:
+            current_app.logger.warn(f"Could not unindex user {user.id}: {e}")
 
 
 def reindex_group(role):
     """Reindex the given role/group."""
-    group_agg = GroupAggregate.from_role(role)
-    current_groups_service.indexer.index(group_agg)
+    if current_groups_service.record_cls.index.exists():
+        try:
+            group_agg = GroupAggregate.from_role(role)
+            current_groups_service.indexer.index(group_agg)
+        except ConflictError as e:
+            current_app.logger.warn(f"Could not reindex group {user.id}: {e}")
 
 
 def unindex_group(role):
     """Unindex the given role/group."""
-    group_agg = GroupAggregate.from_role(role)
-    current_groups_service.indexer.delete(group_agg)
+    if current_groups_service.record_cls.index.exists():
+        try:
+            group_agg = GroupAggregate.from_role(role)
+            current_groups_service.indexer.delete(group_agg)
+        except ConflictError as e:
+            current_app.logger.warn(f"Could not unindex group {user.id}: {e}")
