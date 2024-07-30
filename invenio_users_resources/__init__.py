4f41038914075ce1b9baae32e3ee4f376ea078bf
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-07-30 11:34:48 +0200

    release: v5.3.1

--- invenio_users_resources/__init__.py
+++ invenio_users_resources/__init__.py
@@ -2,6 +2,7 @@
 #
 # Copyright (C) 2022-2024 CERN.
 # Copyright (C) 2022-2023 TU Wien.
+# Copyright (C) 2024 Graz University of Technology.
 #
 # Invenio-Users-Resources is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -9,6 +10,6 @@
 
 """Invenio module providing management APIs for users and roles/groups."""
 
-__version__ = "5.3.0"
+__version__ = "5.3.1"
 
 __all__ = ("__version__",)
