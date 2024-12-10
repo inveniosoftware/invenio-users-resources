7530b9aded4f666714ac68da165128999fa8afcc
Mirek Simek <miroslav.simek@gmail.com>
2024-12-10 14:30:41 +0100

    i18n: Fix untranslated strings in facets
    User and domain translations are not working, even though they are wrapped
    in `_(...)`. The reason is `gettext` is used which performs the translation
    during module initialization and not when the facets are rendered.
    
    This commit fixes this by using `lazy_gettext` instead.
    
    Co-authored-by: Miroslav Simek <miroslav.simek@cesnet.cz>

--- invenio_users_resources/services/users/facets.py
+++ invenio_users_resources/services/users/facets.py
@@ -8,7 +8,7 @@
 
 """Users search facets definitions."""
 
-from invenio_i18n import gettext as _
+from invenio_i18n import lazy_gettext as _
 from invenio_records_resources.services.records.facets import TermsFacet
 
 domain = TermsFacet(
