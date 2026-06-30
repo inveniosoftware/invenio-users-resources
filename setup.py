42fe94eb42cbb662dd9ef217ebecbcb6075ed480
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-06-30 11:13:40 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's only relevant for setuptools
    * translate the i18n/translation mechanisms as well
    * declare conflicting extras for uv
    * format pyproject.toml with Tombi
    
    Co-authored-by: Sam Arbid <samaa2@kth.se>

--- setup.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2022 CERN.
-# SPDX-FileCopyrightText: 2022 TU Wien.
-# SPDX-License-Identifier: MIT
-
-"""Invenio module providing management APIs for users and roles/groups."""
-
-from setuptools import setup
-
-setup()
