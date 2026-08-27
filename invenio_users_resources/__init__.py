fa6f34e1bf5a05f2660d826e21c4420145a44ac4
Sam Arbid <samaa2@kth.se>
2026-08-27 14:13:53 +0200

    📦 release: v12.1.1

--- invenio_users_resources/__init__.py
+++ invenio_users_resources/__init__.py
@@ -1,10 +1,11 @@
 # SPDX-FileCopyrightText: 2022-2026 CERN.
 # SPDX-FileCopyrightText: 2022-2023 TU Wien.
 # SPDX-FileCopyrightText: 2024-2026 Graz University of Technology.
+# SPDX-FileCopyrightText: 2026 KTH Royal Institute of Technology.
 # SPDX-License-Identifier: MIT
 
 """Invenio module providing management APIs for users and roles/groups."""
 
-__version__ = "12.1.0"
+__version__ = "12.1.1"
 
 __all__ = ("__version__",)
